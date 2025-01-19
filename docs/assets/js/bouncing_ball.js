929c9e439237d79184aad2591938a6a80f9da51f
michaelholm6 <mdholm@iastate.edu>
2025-01-19 12:19:35 -0500

    Troubleshooting ball game on ipad

--- docs/assets/js/bouncing_ball.js
+++ docs/assets/js/bouncing_ball.js
@@ -481,7 +481,7 @@ function animate() {
   requestAnimationFrame(animate);
 }
 
-setTimeout(animate(), 5000);
+setTimeout(animate, 750);
 
 function resizeCanvas() {
   //   Update canvas dimensions
