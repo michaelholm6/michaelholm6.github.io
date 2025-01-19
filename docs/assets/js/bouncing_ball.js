c0d0dd7c7d87f5ccb9f9b0fb6ca5f866d22e2252
michaelholm6 <mdholm@iastate.edu>
2025-01-19 14:14:13 -0500

    Troubleshooting ball game on ipad

--- docs/assets/js/bouncing_ball.js
+++ docs/assets/js/bouncing_ball.js
@@ -299,8 +299,8 @@ function updateBall(frameTime) {
     ball.dx = Math.max(-ball.maxSpeed, Math.min(ball.dx, ball.maxSpeed));
     ball.dy = Math.max(-ball.maxSpeed, Math.min(ball.dy, ball.maxSpeed));
 
-    ball.x += ball.dx*frameTime/(1/30);
-    ball.y += ball.dy*frameTime/(1/30);
+    ball.x += ball.dx*frameTime/(1/20);
+    ball.y += ball.dy*frameTime/(1/20);
 
 
     // Bounce off canvas edges
